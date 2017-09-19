851ee9f2d60fb7fd21e62b8ca7d7ca17041cda60
Emma Gibson <egibson@dons.usfca.edu>
2017-09-19 20:01:36 +0000

    add printing first 3 lines to script

--- fasta_QC_checks.sh
+++ fasta_QC_checks.sh
@@ -6,11 +6,30 @@
 
 # unzip data files
 echo "unzipping data files..."
-unzip data/*.zip
+unzip "data/*.zip"
 rm data/*.zip
 echo "unzipped data files"
 
 
 # state purpose of script and date
-echo "Output from preliminary quality control checks on selected .fasta files."
 echo "$(date)"
+echo "Output from preliminary quality control checks on selected .fasta files."
+echo "Output for each .fasta includes:"
+echo "Filename, file size, first three lines, last three lines, the number of"
+echo "sequences in each file, and the sequence identifier lines, sorted alphabetically."
+
+# for loop to do the above for each .fasta
+# run with the argument data/*.fasta for desires result
+for file in $@
+do
+	echo "#####################################################"
+	echo "Output for ${file##*/}"
+	echo "Size = $(cut -b $file)"
+	echo "First three lines:"
+	head -3 $file
+done
+
+#POTENTIAL ISSUE: figure out how to get line breaks between outputs
+
+#POTENTIAL ISSUE: 'head -3' isn't an ideal command because 
+#	some of the files don't start with the sequences
