0a2c55ac5c0fac50e19c2b93ccaf6985fde04b0e
Emma Gibson <egibson@dons.usfca.edu>
2017-09-20 00:18:53 +0000

    improve output readablilty of script

--- fasta_QC_checks.sh
+++ fasta_QC_checks.sh
@@ -22,21 +22,30 @@ echo "sequences in each file, and the sequence identifier lines, sorted alphabet
 # run with the argument data/*.fasta for desires result
 for file in $@
 do
+	echo " "
+	echo " "
 	echo "#####################################################"
+# prints file name without suffixes (data/example.fasta becomes ecample.fasta)
 	echo "Output for ${file##*/}"
-	echo "Size = $(du -h "$file")"
+	echo " "
+# prints file size without file name
+	echo "Size = $(du -h "$file" | cut -f1)"
+	echo " "
+# counts number of sequences in file
 	echo "Number of sequences = $(grep -c "^>" $file)"
-	echo "Sequence Names:"
-	echo "$(grep ">" $file | sort)"
+	echo " "
+# lists first and last 3 lines of each file
 	echo "First three lines:"
 	head -3 $file
+	echo " "
 	echo "Last three lines:"
 	tail -3 $file
+	echo " "
+# lists sequence names in alphabetical order
+        echo "Sequence Names:"
+        echo "$(grep ">" -f2 $file | sort)"
 done
-
-#POTENTIAL ISSUE: figure out how to get line breaks between outputs
 
 #POTENTIAL ISSUE: 'head -3' isn't an ideal command because 
 #	some of the files don't start with the sequences
-
 #POTENTIAL ISSUE: 'tail -3' has similar issues to 'head -3'
